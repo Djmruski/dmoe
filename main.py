273301f49a32b9640f327521c7c10a0c57e5177b
Drew Williams <drew.williams.hk@gmail.com>
2024-02-01 14:43:12 +0000

    started testing dytox model

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 
 import argparse
+import torch
 
 from classifier import Classifier
 from dytox import DyTox
 
 def main(args):
     num_classes = 10
-    model = DyTox(num_classes, dim=405, B=32, N=9, C=45, embed_dim=78)
-    model.head = Classifier() # TODO:
+    model = DyTox(num_classes, dim=405, B=32, C=45, embed_dim=78)
+    # model.head = Classifier() # TODO:
 
-    
+    x = torch.randn(32, 9, 45)
+    out = model(x)
+    print(out[0].shape)
+
 
 if "__main__" in __name__:
     parser = argparse.ArgumentParser()
